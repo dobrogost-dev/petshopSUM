4f7e803fd7506fec801c7497af8fb1c0ff7b466b
Dobrogost <dobrogost.adrian@gmail.com>
2023-06-17 23:00:47 +0200

    Dodanie przycisków dodaj i usuń

--- src/main/java/frame/StoreFrame.java
+++ src/main/java/frame/StoreFrame.java
@@ -5,13 +5,18 @@ import sum.product.Product;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-public class StoreFrame extends MainFrame{
+public class StoreFrame extends MainFrame {
     private JTable productTable;
+    private JButton addButton;
+    private JButton deleteButton;
+
     public StoreFrame(Application app, int x, int y, Dimension size) {
         super(app, x, y, size);
         this.view = View.STORE;
@@ -20,24 +25,29 @@ public class StoreFrame extends MainFrame{
 
         // Ustawianie JTable z modelem
         productTable = new JTable(model);
-        // Dodanie przycisku "Dodaj" na górze tabeli
-        JButton addButton = new JButton("Dodaj");
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Tutaj można dodać kod obsługi kliknięcia przycisku "Dodaj"
-            }
-        });
+
+        // Dodanie przycisku "Dodaj" i "Usuń" na górze tabeli
+        addButton = new JButton("Dodaj");
+        deleteButton = new JButton("Usuń");
 
         // Ustawianie JScrollPane jako widoku dla JTable
         JScrollPane scrollPane = new JScrollPane(productTable);
 
         // Ustawianie układu i dodawanie komponentów do panelu głównego
+        mainPanel.setLayout(new BorderLayout());
+
+        // Dodawanie przycisków "Dodaj" i "Usuń" obok siebie
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        buttonPanel.add(addButton);
+        buttonPanel.add(deleteButton);
+        mainPanel.add(buttonPanel, BorderLayout.NORTH);
+
         mainPanel.add(scrollPane, BorderLayout.CENTER);
-        setLocationRelativeTo(null);
 
         this.initialize();
     }
+
     class ProductTableModel extends AbstractTableModel {
         private List<Product> products;
         private String[] columnNames = {"ID", "Amount", "Price", "Name", "Country", "Species"};
@@ -59,22 +69,15 @@ public class StoreFrame extends MainFrame{
         @Override
         public Object getValueAt(int row, int column) {
             Product product = products.get(row);
-            switch (column) {
-                case 0:
-                    return product.getId();
-                case 1:
-                    return product.getAmount();
-                case 2:
-                    return product.getPrice();
-                case 3:
-                    return product.getName();
-                case 4:
-                    return product.getCountry();
-                case 5:
-                    return product.getSpecies().getName() + " - " + product.getSpecies().getRace();
-                default:
-                    return null;
-            }
+            return switch (column) {
+                case 0 -> product.getId();
+                case 1 -> product.getAmount();
+                case 2 -> product.getPrice();
+                case 3 -> product.getName();
+                case 4 -> product.getCountry();
+                case 5 -> product.getSpecies().getName() + " - " + product.getSpecies().getRace();
+                default -> null;
+            };
         }
 
         @Override
